3a8f56c7fed40de38b4b96e17ad5a618a15643de
Erica <ericalee.webdev@gmail.com>
2025-11-11 12:31:25 -0500

    create-session route file correct

--- app/api/create-session/route.ts
+++ app/api/create-session/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST() {
